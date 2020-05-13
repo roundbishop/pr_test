30d76c5e6638e4421d3c2f1eb580c1f64a022ee5
test <test222>
2020-05-14 08:15:36 +0900

    1111

--- test.js
+++ test.js
@@ -10,12 +10,4 @@ console.log('test333');
 
 console.log('test444');
 
-console.log('test5555');
-
-console.log('test6666');
-
-console.log('test7777');
-
-console.log('test77778');
-
-console.log('test77779');
+console.log("new feautre");
